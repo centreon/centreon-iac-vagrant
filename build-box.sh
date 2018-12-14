6bdfd2ec98123f9ab8eb657bd182bc27b7e783ff
Luiz Gustavo S. Costa <luizgustavo@luizgustavo.pro.br>
2018-12-14 16:29:58 +0000

    Update build-box.sh
    fix the branch to use

--- build-box.sh
+++ build-box.sh
@@ -2,7 +2,7 @@
 echo "Start Vagrant machine to build Box ..."
 vagrant up
 echo "Preparing module Ansible to Deploy a new installation of Centreon ..."
-git clone --single-branch -b new_wrapper https://github.com/centreon/centreon-ansible
+git clone --single-branch -b master https://github.com/centreon/centreon-ansible
 echo "Start installation of Centreon using Ansible ..."
 cd centreon-ansible
 ansible-playbook -i ../hosts.ansible ../deploy.yml
